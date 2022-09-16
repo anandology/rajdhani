d947137bde4cc6a0262d0111d296485beb63f2a2
Anand Chitipothu <anandology@gmail.com>
2022-09-16 16:53:31 +0530

    Added placeholders and implement ticket class support.

--- rajdhani/db.py
+++ rajdhani/db.py
@@ -4,6 +4,7 @@ Module to interact with the database.
 from sqlalchemy import (
     create_engine, MetaData, Table,
     select, func, or_, literal)
+from . import placeholders
 from . import config
 from . import db_ops
 
@@ -23,6 +24,7 @@ def search_stations(q):
     The q is the few characters of the station name or
     code entered by the user.
     """
+    return placeholders.AUTOCOMPLETE_STATIONS
     s = station_table
 
     query = (
@@ -35,7 +37,7 @@ def search_stations(q):
     )
     return [dict(row) for row in query.execute()]
 
-def search_trains(from_station, to_station, date, ticket_class):
+def search_trains(from_station, to_station, date, ticket_class=None):
     """Returns all the trains that source to destination stations on
     the given date. When ticket_class is provided, this should return
     only the trains that have that ticket class.
@@ -56,6 +58,16 @@ def search_trains(from_station, to_station, date, ticket_class):
             t.c.duration_m)
         .where(t.c.from_station_code==from_station)
         .where(t.c.to_station_code==to_station))
+    if ticket_class:
+        columns = {
+            "SL": t.c.sleeper,
+            "3A": t.c.third_ac,
+            "2A": t.c.second_ac,
+            "1A": t.c.first_ac,
+            "FC": t.c.first_class,
+            "CC": t.c.chair_car,
+        }
+        q = q.where(columns[ticket_class]==1)
     return q.execute().all()
 
     # TODO: make a db query to get the matching trains
